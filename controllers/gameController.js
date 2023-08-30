408b6be0ecbce2705138134dbf5ced7b7020cf9c
Josiah Morris <josiahbmorris@hotmail.com>
2023-08-31 04:16:22 +0900

    Added index controller to gameController, and accompanying index view

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -1,5 +1,67 @@
+const Game = require('../models/game')
+const Platform = require('../models/platform')
+const Accessory = require('../models/accessory')
+const Merchandise = require('../models/merchandise')
+const Genre = require('../models/genre')
+const Developer = require('../models/developer')
+
 exports.index = async (req, res, next) => {
-  res.render('index', { title: 'Not EB Games' })
+  const [
+    gameCount,
+    gameStockCount,
+    platformCount,
+    platformStockCount,
+    accessoryCount,
+    accessoryStockCount,
+    merchandiseCount,
+    merchandiseStockCount,
+    genreCount,
+    developerCount,
+  ] = await Promise.all([
+    Game.countDocuments({}).exec(),
+    Game.aggregate([{
+      $group: { _id: '', numberInStock: {$sum: '$numberInStock'}
+      }}, {
+      $project: { _id: 0, numberInStock: '$numberInStock'
+      }
+    }]),
+    Platform.countDocuments({}).exec(),
+    Platform.aggregate([{
+      $group: { _id: '', numberInStock: {$sum: '$numberInStock'}
+      }}, {
+      $project: { _id: 0, numberInStock: '$numberInStock'
+      }
+    }]),
+    Accessory.countDocuments({}).exec(),
+    Accessory.aggregate([{
+      $group: { _id: '', numberInStock: {$sum: '$numberInStock'}
+      }}, {
+      $project: { _id: 0, numberInStock: '$numberInStock'
+      }
+    }]),
+    Merchandise.countDocuments({}).exec(),
+    Merchandise.aggregate([{
+      $group: { _id: '', numberInStock: {$sum: '$numberInStock'}
+      }}, {
+      $project: { _id: 0, numberInStock: '$numberInStock'
+      }
+    }]),
+    Genre.countDocuments({}).exec(),
+    Developer.countDocuments({}).exec(),
+  ])
+  res.render('index', {
+    title: 'Not EB Games',
+    gameCount,
+    gameStockCount,
+    platformCount,
+    platformStockCount,
+    accessoryCount,
+    accessoryStockCount,
+    merchandiseCount,
+    merchandiseStockCount,
+    genreCount,
+    developerCount
+  })
 }
 
 exports.gameCreateGet = async(req, res, next) => {}
