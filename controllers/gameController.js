7a5b6e2ea5ae0d4669dd95b6f7e2be7a92accf00
Josiah Morris <josiahbmorris@hotmail.com>
2023-09-01 04:50:50 +0900

    Added Game detail view

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -73,7 +73,14 @@ exports.gameDeletePost = async(req, res, next) => {}
 exports.gameUpdateGet = async(req, res, next) => {}
 exports.gameUpdatePost = async(req, res, next) => {}
 
-exports.gameDetail = async(req, res, next) => {}
+exports.gameDetail = async(req, res, next) => {
+  const game = await Game.findById(req.params.id)
+    .populate('developer')
+    .populate('genre')
+    .populate('platform')
+    .exec()
+  res.render('game_detail', { title: game.title + ' details', game})
+}
 exports.gameList = async(req, res, next) => {
   const allGames = await Game.find().exec()
   res.render('game_list', { title: 'Game List', allGames})
