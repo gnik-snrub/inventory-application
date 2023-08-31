ff129321eca71debccfc42b6521014f4e2183c4f
Josiah Morris <josiahbmorris@hotmail.com>
2023-09-01 06:00:17 +0900

    Added Developer list view

--- controllers/developerController.js
+++ controllers/developerController.js
@@ -9,4 +9,7 @@ exports.developerUpdateGet = async(req, res, next) => {}
 exports.developerUpdatePost = async(req, res, next) => {}
 
 exports.developerDetail = async(req, res, next) => {}
-exports.developerList = async(req, res, next) => {}
+exports.developerList = async(req, res, next) => {
+  const allDevelopers = await Developer.find().exec()
+  res.render('developer_list', { title: 'Developer List', allDevelopers })
+}
