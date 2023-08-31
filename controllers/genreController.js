f554ddd3914d8cb34aefedf375ae858368af2e4c
Josiah Morris <josiahbmorris@hotmail.com>
2023-09-01 05:00:04 +0900

    Added Genre list view

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,3 +1,4 @@
+const Genre = require('../models/genre')
 
 exports.genreCreateGet = async(req, res, next) => {}
 exports.genreCreatePost = async(req, res, next) => {}
@@ -9,4 +10,7 @@ exports.genreUpdateGet = async(req, res, next) => {}
 exports.genreUpdatePost = async(req, res, next) => {}
 
 exports.genreDetail = async(req, res, next) => {}
-exports.genreList = async(req, res, next) => {}
+exports.genreList = async(req, res, next) => {
+  const allGenres = await Genre.find().exec()
+  res.render('genre_list', { title: 'Genre List', allGenres })
+}
