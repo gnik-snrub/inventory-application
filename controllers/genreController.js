5bf6162d49a0ec44e1a81f0361678d4c9aad4eac
Josiah Morris <josiahbmorris@hotmail.com>
2023-09-01 05:09:08 +0900

    Added Genre detail view

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,4 +1,5 @@
 const Genre = require('../models/genre')
+const Game = require('../models/game')
 
 exports.genreCreateGet = async(req, res, next) => {}
 exports.genreCreatePost = async(req, res, next) => {}
@@ -9,7 +10,13 @@ exports.genreDeletePost = async(req, res, next) => {}
 exports.genreUpdateGet = async(req, res, next) => {}
 exports.genreUpdatePost = async(req, res, next) => {}
 
-exports.genreDetail = async(req, res, next) => {}
+exports.genreDetail = async(req, res, next) => {
+  const [genre, genreGames] = await Promise.all([
+    Genre.findById(req.params.id).exec(),
+    Game.find({ genre: req.params.id }, 'title summary').exec()
+  ])
+  res.render('genre_detail', { title: genre.name + ' details', genre, genreGames})
+}
 exports.genreList = async(req, res, next) => {
   const allGenres = await Genre.find().exec()
   res.render('genre_list', { title: 'Genre List', allGenres })
