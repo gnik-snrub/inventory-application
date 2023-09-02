5e578da08c4ab5cc990d327eeadeab30fec1000b
Josiah Morris <josiahbmorris@hotmail.com>
2023-09-03 01:23:05 +0900

    Added Merchandise detail view

--- controllers/merchandiseController.js
+++ controllers/merchandiseController.js
@@ -1,3 +1,4 @@
+const Merchandise = require('../models/merchandise')
 
 exports.merchandiseCreateGet = async(req, res, next) => {}
 exports.merchandiseCreatePost = async(req, res, next) => {}
@@ -8,7 +9,10 @@ exports.merchandiseDeletePost = async(req, res, next) => {}
 exports.merchandiseUpdateGet = async(req, res, next) => {}
 exports.merchandiseUpdatePost = async(req, res, next) => {}
 
-exports.merchandiseDetail = async(req, res, next) => {}
+exports.merchandiseDetail = async(req, res, next) => {
+  const merchandise = await Merchandise.findById(req.params.id).populate('relatedGames').exec()
+  res.render('merchandise_detail', { title: merchandise.name + ' details', merchandise})
+}
 exports.merchandiseList = async(req, res, next) => {
   const allMerchandise = await Merchandise.find().exec()
   res.render('merchandise_list', { title: 'All Merchandise', allMerchandise })
