be106e27a37c0be8f0c4824ad75c654aef6b3c25
Josiah Morris <josiahbmorris@hotmail.com>
2023-09-03 01:24:56 +0900

    Added Platform detail view

--- controllers/platformController.js
+++ controllers/platformController.js
@@ -1,4 +1,6 @@
 const Platform = require('../models/platform')
+const Game = require('../models/game')
+const Accessory = require('../models/accessory')
 
 exports.platformCreateGet = async(req, res, next) => {}
 exports.platformCreatePost = async(req, res, next) => {}
@@ -9,8 +11,14 @@ exports.platformDeletePost = async(req, res, next) => {}
 exports.platformUpdateGet = async(req, res, next) => {}
 exports.platformUpdatePost = async(req, res, next) => {}
 
-exports.platformDetail = async(req, res, next) => {}
-exports.platformList = async(req, res, next) => {}
+exports.platformDetail = async(req, res, next) => {
+  const [platform, platformGames, platformAccessories] = await Promise.all([
+    Platform.findById(req.params.id).exec(),
+    Game.find({platform: req.params.id}).exec(),
+    Accessory.find({compatiblePlatforms: req.params.id}).exec()
+  ])
+  res.render('platform_detail', { title: platform.name + ' details', platform, platformGames, platformAccessories })
+}
 exports.platformList = async(req, res, next) => {
   const allPlatforms = await Platform.find().exec()
   res.render('platform_list', { title: 'Platform List', allPlatforms })
