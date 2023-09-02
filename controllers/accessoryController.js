465c5166dc3aab7965fc5a3322c2f1edda1eac29
Josiah Morris <josiahbmorris@hotmail.com>
2023-09-03 01:21:57 +0900

    Added Accessory detail view

--- controllers/accessoryController.js
+++ controllers/accessoryController.js
@@ -1,3 +1,4 @@
+const Accessory = require('../models/accessory')
 
 exports.accessoryCreateGet = async(req, res, next) => {}
 exports.accessoryCreatePost = async(req, res, next) => {}
@@ -8,7 +9,10 @@ exports.accessoryDeletePost = async(req, res, next) => {}
 exports.accessoryUpdateGet = async(req, res, next) => {}
 exports.accessoryUpdatePost = async(req, res, next) => {}
 
-exports.accessoryDetail = async(req, res, next) => {}
+exports.accessoryDetail = async(req, res, next) => {
+  const accessory = await Accessory.findById(req.params.id).populate('compatiblePlatforms').exec()
+  res.render('accessory_detail', { title: accessory.name + ' details', accessory})
+}
 exports.accessoryList = async(req, res, next) => {
   const allAccessories = await Accessory.find().exec()
   res.render('accessory_list', { title: 'All Accessories', allAccessories })
