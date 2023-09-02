868e63c0b54ec5af125c63b3b54ad0c6d5ef1f14
Josiah Morris <josiahbmorris@hotmail.com>
2023-09-03 01:24:49 +0900

    Added Platform list view

--- controllers/platformController.js
+++ controllers/platformController.js
@@ -1,3 +1,4 @@
+const Platform = require('../models/platform')
 
 exports.platformCreateGet = async(req, res, next) => {}
 exports.platformCreatePost = async(req, res, next) => {}
@@ -10,3 +11,7 @@ exports.platformUpdatePost = async(req, res, next) => {}
 
 exports.platformDetail = async(req, res, next) => {}
 exports.platformList = async(req, res, next) => {}
+exports.platformList = async(req, res, next) => {
+  const allPlatforms = await Platform.find().exec()
+  res.render('platform_list', { title: 'Platform List', allPlatforms })
+}
