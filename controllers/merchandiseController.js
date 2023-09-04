77cc2173600507f55f7bf57fe79f9f54d4b68bd9
Josiah Morris <josiahbmorris@hotmail.com>
2023-09-05 02:20:08 +0900

    Added Merchandise Create view and controller

--- controllers/merchandiseController.js
+++ controllers/merchandiseController.js
@@ -1,9 +1,44 @@
 const Merchandise = require('../models/merchandise')
+const Game = require('../models/game')
 
+const { body, validationResult } = require('express-validator')
 const asyncHandler = require('express-async-handler')
 
-exports.merchandiseCreateGet = asyncHandler(async(req, res, next) => {})
-exports.merchandiseCreatePost = asyncHandler(async(req, res, next) => {})
+exports.merchandiseCreateGet = asyncHandler(async(req, res, next) => {
+  const allGames = await Game.find().exec()
+  res.render('merchandise_form', { title: 'Create Merchandise', allGames })
+})
+exports.merchandiseCreatePost = [
+  body('name', 'Title must not be empty').trim().notEmpty(),
+  body('price', 'Accessory must have price higher than 0').notEmpty().isInt({ min: 1 }),
+  body('summary', 'Accessory must have summary').trim().notEmpty(),
+  body('numberInStock', 'Accessory stock count must be included (0 is acceptable)').notEmpty(),
+  body('relatedGames.*').escape(),
+  asyncHandler(async(req, res, next) => {
+    const errors = validationResult(req)
+
+    const merchandise = new Merchandise({
+      name: req.body.name,
+      price: req.body.price,
+      summary: req.body.summary,
+      numberInStock: req.body.numberInStock,
+      relatedGames: req.body.relatedGames,
+    })
+
+    if (!errors.isEmpty()) {
+      const allGames = await Game.find().exec()
+      res.render('merchandise_form', {
+        title: 'Create Merchandise',
+        allGames,
+        merchandise
+      })
+      return
+    } else {
+      await merchandise.save()
+      res.redirect(merchandise.url)
+    }
+  })
+]
 
 exports.merchandiseDeleteGet = asyncHandler(async(req, res, next) => {})
 exports.merchandiseDeletePost = asyncHandler(async(req, res, next) => {})
