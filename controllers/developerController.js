f7a592a36087fcac674d1dbfb1a6664f60eb563e
Josiah Morris <josiahbmorris@hotmail.com>
2023-09-05 02:19:21 +0900

    Added Developer Create view and controller

--- controllers/developerController.js
+++ controllers/developerController.js
@@ -1,10 +1,37 @@
 const Developer = require('../models/developer')
 const Game = require('../models/game')
 
+const { body, validationResult } = require('express-validator')
 const asyncHandler = require('express-async-handler')
 
-exports.developerCreateGet = asyncHandler(async(req, res, next) => {})
-exports.developerCreatePost = asyncHandler(async(req, res, next) => {})
+exports.developerCreateGet = asyncHandler(async(req, res, next) => {
+  res.render('developer_form', { title: 'Create Developer'})
+})
+exports.developerCreatePost = [
+  body('name', 'Developer name must not be empty').trim().isLength({ min: 1 }).escape(),
+  asyncHandler(async(req, res, next) => {
+    const errors = validationResult(req)
+
+    const developer = new Developer({name: req.body.name})
+
+    if (!errors.isEmpty()) {
+      res.render('developer_form', {
+        title: 'Create Developer',
+        developer,
+        errors: errors.array()
+      })
+      return
+    } else {
+      const developerExists = await Developer.findOne({name: req.body.name}).collation({ locale: 'en', strength: 2 }).exec()
+      if (developerExists) {
+        res.redirect(developerExists.url)
+      } else {
+        await developer.save()
+        res.redirect(developer.url)
+      }
+    }
+  })
+]
 
 exports.developerDeleteGet = asyncHandler(async(req, res, next) => {})
 exports.developerDeletePost = asyncHandler(async(req, res, next) => {})
