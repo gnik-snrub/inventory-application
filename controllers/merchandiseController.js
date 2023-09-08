ede82d3fecbb33a44d5adb0498f3af7db46a3b50
Josiah Morris <josiahbmorris@hotmail.com>
2023-09-09 01:46:34 +0900

    Added Merchandise Update controller

--- controllers/merchandiseController.js
+++ controllers/merchandiseController.js
@@ -57,8 +57,58 @@ exports.merchandiseDeletePost = asyncHandler(async(req, res, next) => {
   res.redirect('/shop/merchandises')
 })
 
-exports.merchandiseUpdateGet = asyncHandler(async(req, res, next) => {})
-exports.merchandiseUpdatePost = asyncHandler(async(req, res, next) => {})
+exports.merchandiseUpdateGet = asyncHandler(async(req, res, next) => {
+  const [merchandise, allGames] = await Promise.all([
+    Merchandise.findById(req.params.id),
+    Game.find()
+  ])
+  for (const game of allGames) {
+    if (merchandise.relatedGames.includes(game._id)) {
+      game.checked = 'true'
+    }
+  }
+  res.render('merchandise_form', { title: 'Update Merchandise', merchandise, allGames })
+})
+exports.merchandiseUpdatePost = [
+  body('name', 'Title must have between 3 and 100 characters').trim().isLength({min: 3, max: 100}),
+  body('price', 'Merchandise must have a price').notEmpty(),
+  body('summary', 'Merchandise must have between 3 and 200 characters').trim().isLength({min: 3, max: 200}),
+  body('numberInStock', 'Merchandise stock count must be included').notEmpty(),
+  body('relatedGames.*').escape(),
+  asyncHandler(async(req, res, next) => {
+    const errors = validationResult(req)
+
+    const merchandise = new Merchandise({
+      name: req.body.name,
+      price: req.body.price,
+      summary: req.body.summary,
+      numberInStock: req.body.numberInStock,
+      relatedGames: req.body.relatedGames,
+      _id: req.params.id
+    })
+
+    if (!errors.isEmpty()) {
+      const allGames = await Game.find().exec()
+
+      for (const game of allGames) {
+        if (merchandise.relatedGames.includes(game._id)) {
+          game.checked = 'true'
+        }
+      }
+
+      res.render('merchandise_form', {
+        title: 'Create Merchandise',
+        errors: errors.array(),
+        allGames,
+        merchandise
+      })
+      return
+    } else {
+      await Merchandise.findByIdAndUpdate(req.params.id, merchandise)
+      res.redirect(merchandise.url)
+    }
+  })
+]
 
 exports.merchandiseDetail = asyncHandler(async(req, res, next) => {
   const merchandise = await Merchandise.findById(req.params.id).populate('relatedGames').exec()
